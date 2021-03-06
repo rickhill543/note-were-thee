5723b179d70622bd5d53351d711270512b57e62a
jWalker <rickhill543@gmail.com>
2021-03-06 17:07:02 -0500

    port fix

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-app.listen(3001, () => {
-    console.log(`API server is a go. Port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server is a go. Port ${PORT}!`);
   });
