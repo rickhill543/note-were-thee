6467442c51daddaf96458bcebe3bf70a3314bd70
jWalker <rickhill543@gmail.com>
2021-03-07 19:02:11 -0500

    setup save functionality

--- routes/apiRoutes/notesRoute.js
+++ routes/apiRoutes/notesRoute.js
@@ -1,15 +1,29 @@
 var uniqid = require('uniqid');
 const router = require('express').Router();
-const { getNoteIndex } = require('../../lib/notey');
+const { createNewNote, getNoteIndex } = require('../../lib/notey');
 const { notesArray } = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
   let results = notesArray;
-  // if (req.query) {
-  //   results = filterByQuery(req.query, results);
-  // }
   console.log("req.query: " + req.query);
   res.send(results);
+});
+
+router.get('/notes/:id', (req, res) => {
+  const result = getNoteIndex(req.params.id, notesArray);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+router.post('/notes', (req, res) => {
+    req.body.id = uniqid();
+    console.log("req.body: " + req.body.id);
+    console.log(req.body.id);
+    const note = createNewNote(req.body, notesArray);
+    res.json(note);
 });
 
 router.get('/notes/:id', (req, res) => {
