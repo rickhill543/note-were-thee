3878b76720f6abac44ae514c78a1511e1be40e32
jWalker <rickhill543@gmail.com>
2021-03-07 19:09:32 -0500

    setup delete note feature

--- routes/apiRoutes/notesRoute.js
+++ routes/apiRoutes/notesRoute.js
@@ -1,6 +1,6 @@
 var uniqid = require('uniqid');
 const router = require('express').Router();
-const { createNewNote, getNoteIndex } = require('../../lib/notey');
+const { createNewNote, getNoteIndex, deleteNote } = require('../../lib/notey');
 const { notesArray } = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
@@ -26,13 +26,11 @@ router.post('/notes', (req, res) => {
     res.json(note);
 });
 
-router.get('/notes/:id', (req, res) => {
-  const result = getNoteIndex(req.params.id, notesArray);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
+router.delete('/notes/:id', (req, res) => {
+  console.log("req body in delete route: " + req.body);
+  const noteIndex = getNoteIndex(req.params.id, notesArray);
+  const dlt = deleteNote(noteIndex, notesArray);
+  res.json(dlt);
 });
 
 module.exports = router;
